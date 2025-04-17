d7cc6d25a89ebac36a12e0df3ea4ba4aaef3a37b
Luca Löschmann <146280625+LucaLoeschmann@users.noreply.github.com>
2025-04-17 12:22:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ outfield_df = outfield_df[outfield_df["90s"] > 3]
 
 meta_columns = ["Player Name", "Nationality", "Position", "Team", "Competition", "Age"]
 features_for_clustering = [
-    "Goals-PK", "Shots", "xG", "npxG", "npxG+xAG", "xAG", "Assists", "Key Passes", "SCA", "GCA",
+    "Goals-PK", "Shots","npxG", "xAG", "Assists", "Key Passes", "SCA", "GCA",
     "Passes Attempted", "Pass Completion Percentage", "Passes into Penalty Area", "Final Third Passes",
     "Through Balls", "Progressive Passes", "Switches", "Crosses",
     "Progressive Carries", "Carries", "Carries Into Final 3rd", "Carries Into Penalty Area",
@@ -70,7 +70,7 @@ outfield_df['Player_ID'] = outfield_df['Player Name'] + " | " + outfield_df['Tea
 
 # --- Radar Chart Feature Set ---
 radar_columns_outfield = [
-    "Goals-PK", "npxG", "Shots", "Assists", "xAG", "npxG+xAG", "SCA",
+    "Goals-PK", "npxG", "Shots", "Assists", "xAG", "SCA",
     "Passes Attempted", "Pass Completion Percentage", "Progressive Passes",
     "Progressive Carries", 'Touches Attacking Penalty Area',
     "Progressive Passes Received", "Tackles", "Interceptions",
